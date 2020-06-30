238a75ad4efad4d1846d74c6a99f8d9c99030f6b
bsparnochia <brian.sparnochia@gmail.com>
2020-06-29 22:29:13 -0300

    Nuevo ejemplo en main.cpp del uso de la colección Stack

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "Stack.h"
+
+using namespace std;
 
 int main(){
+	Stack<char> pila;
+
+	cout << "lectura normal : ";
+	pila.apilar('R');
+	cout << pila.getTope();
+	pila.apilar('O');
+	cout << pila.getTope();
+	pila.apilar('M');
+	cout << pila.getTope();
+	pila.apilar('A');
+	cout << pila.getTope() << endl;
+
+	cout << "lectura al revés : ";
+	while (!pila.estaVacia()){
+		cout << pila.desapilar();
+	}
 	return 0;
 }
 
